2f6c76578a138f813c12086d7bce1f7167340a4c
Javier Barrera <js.barrerat1@uniandes.edu.co>
2024-03-24 11:08:32 -0500

    Feat: modularize the plugins

--- init.lua
+++ init.lua
@@ -1,28 +1,3 @@
---[[
--- In this file is my personal configutarion to Nvim.
---
--- You can find how to do it by yourself in the next YouTube video
--- https://www.youtube.com/watch?v=zHTeCSVAFNY
---]]
-
-
--------------------------
--- Basic configuration
--------------------------
-
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set autoindent")
-vim.cmd("set number")
-vim.cmd("set mouse=a")
-vim.cmd("syntax on")
-
--------------------------
--- Install Lazy
--------------------------
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -36,65 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--------------------------
--- Define plugins
--------------------------
-
-local plugins = {
-  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000
-  }, {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5", 
-    dependencies = { "nvim-lua/plenary.nvim" }
-  }, {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate"
-  }, {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    }
-  }
-}
-local opts = {}
-
-require("lazy").setup(plugins, opts)
-require("gruvbox").setup();
-local builtin = require("telescope.builtin")
---require("catppuccin").setup()
-
--------------------------
--- Keymap set
--------------------------
-
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal right<CR>', {})
-vim.keymap.set('n', '<C-w>', ':wq<CR>', {})
-vim.keymap.set('n', '<C-q>', ':q<CR>', {})
-vim.keymap.set('n', '<C-s>', ':w<CR>', {})
-
--------------------------
--- Config Treesitter
--------------------------
-
-local configs = require("nvim-treesitter.configs")
-
-configs.setup({
-  ensure_installed = {"lua", "java", "javascript", "typescript", "c"},
-  sync_install = false,
-  highlight = { enable = true },
-  indent = { enable = true },
-})
-
--------------------------
--- Theme configuration
--------------------------
-
-vim.cmd("colorscheme gruvbox")
+require("vim-options") -- vim configs
+require("vim-keymaps") -- vim keymaps configs
+require("lazy").setup("plugins") -- lazy vim config
 
